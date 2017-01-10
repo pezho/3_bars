14f082a06d3aed382a5363aed17af3d195f462f5
pezho <pezho.i@mail.ru>
2017-01-10 21:25:53 +0200

    first try

--- bars.py
+++ bars.py
@@ -1,21 +1,26 @@
 import json
-
+import math
 
 def load_data(filepath):
-    pass
+    with open(filepath) as data:
+        d = json.loads(data.read())
+    return d
 
 
 def get_biggest_bar(data):
-    pass
+    return(max(data, key=lambda x: x['SeatsCount']))
 
 
 def get_smallest_bar(data):
-    pass
+    return(min(data, key=lambda x: x['SeatsCount']))
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    return(min(data, key=lambda x: math.hypot(float(x['Latitude_WGS84']) - latitude, float(float(x['Longitude_WGS84']) - longitude))))
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data('data-2897-2016-11-23.json')
+    print(get_biggest_bar(data))
+    print(get_smallest_bar(data))
+    print(get_closest_bar(data), float(input()), float(input()))
