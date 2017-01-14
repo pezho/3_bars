248cdfab43469688981fcd3908b249443401307a
pezho <pezho.i@mail.ru>
2017-01-14 19:19:21 +0200

    Update bars.py

--- bars.py
+++ bars.py
@@ -1,25 +1,36 @@
+import argparse
 import json
 import math
+
 
 def load_data(filepath):
     with open(filepath) as moscow_bars:
         return json.loads(moscow_bars.read())
 
 
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', help='path to json file')
+    return parser.parse_args().f
+
+
 def get_biggest_bar(data):
-    return(max(data, key=lambda x: x['SeatsCount']))
+    return (max(data, key=lambda x: x['SeatsCount']))
 
 
 def get_smallest_bar(data):
-    return(min(data, key=lambda x: x['SeatsCount']))
+    return (min(data, key=lambda x: x['SeatsCount']))
 
 
 def get_closest_bar(data, longitude, latitude):
-    return(min(data, key=lambda x: math.hypot(float(x['Latitude_WGS84']) - latitude, float(float(x['Longitude_WGS84']) - longitude))))
+    return (min(data, key=lambda x: math.hypot(float(x['Latitude_WGS84']) - latitude,
+                                               float(float(x['Longitude_WGS84']) - longitude))))
 
 
 if __name__ == '__main__':
-    moscow_bars = load_data('data-2897-2016-11-23.json')
-    print(get_biggest_bar(moscow_bars))
-    print(get_smallest_bar(moscow_bars))
-    print(get_closest_bar(moscow_bars), float(input()), float(input()))
+    path = create_parser()
+    moscow_bars = load_data(path)
+    print('The most biggest bar: {}'.format(get_biggest_bar(moscow_bars)))
+    print('The most smallest bar: {}'.format(get_smallest_bar(moscow_bars)))
+    print('Nearest bar is: {}'.format(
+        get_closest_bar(moscow_bars, float(input('Enter your longitude: ')), float(input('Enter your latitude: ')))))
